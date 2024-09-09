11d01f74c981147a0341d179ebccbe83c8be5003
Brenda <brendak-alves@outlook.com>
2024-09-09 17:12:51 -0300

    Complete all challenges.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,27 @@
+function getRandomInt() {
+    return Math.floor(Math.random() *999);
+}
+
 const repeatString = function(string, num) {
     string;
     +num;
 
     let result = "";
 
+    if (num < 0) {
+        result = 'ERROR';
+    }
+
     for (let i = 1; i <= num; i++) {
         result += string;
+
     }
 
     return result;
+ 
 };
 
-repeatString('hey', 3);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
