85840feee858042740ef6b1480ba97e4c273d95b
Brenda <brendak-alves@outlook.com>
2024-09-09 16:43:15 -0300

    Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    string;
+    +num;
 
+    let result = "";
+
+    for (let i = 1; i <= num; i++) {
+        result += string;
+    }
+
+    return result;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
